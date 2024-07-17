fd7496e959fc415e59509f9a4cd86fdd09f68b89
MatiasCollaguazo <mcollagu@espol.edu.ec>
2024-07-17 16:46:23 -0500

    Initial Files

---
+++ src/main/java/ec/edu/espol/taller08_patronescec/Taller08_PatronesCEC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.taller08_patronescec;
+
+/**
+ *
+ * @author matia
+ */
+public class Taller08_PatronesCEC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
